cef5689ab3e679db07d2169601fab1b23373831c
rmfls0606 <rmfls0606@naver.com>
2024-03-20 16:53:55 +0900

    Initial Commit

---
+++ InstaGram/InstaGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaGramApp.swift
+//  InstaGram
+//
+//  Created by 이상민 on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
